71d546a6a2296d307d31bd3f73cbf51aa2a39d1e
sqrts123 <wilcorhy001@warren.k12.in.us>
2024-10-01 18:24:16 -0400

    Test
    Just a test :0

--- maine.py
+++ maine.py
@@ -1,1 +1,9 @@
 print("This is a random thing I made for no reason at all :D")
+#Python is fun >:3
+
+Input_Number1 = input("Type a number.")
+Input_Number2 = input("Type another number.")
+Type = input(+)
+
+
+print("...", Input_Number1, Type, Input_Number2, "equals", Input_Number1 + Input_Number2)
